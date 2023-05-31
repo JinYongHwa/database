d7487182dcd6fd32f5e3b39bb2f50e93b3b0e8b1
jinyonghwa <jinyonghwa@kakao.com>
2023-05-31 12:17:55 +0900

    add index

---
+++ index/employee.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `employee` (
+  `empno` int(11) NOT NULL,
+  `empname` varchar(45) DEFAULT NULL,
+  `title` varchar(45) DEFAULT NULL,
+  `dno` varchar(45) DEFAULT NULL,
+  `salary` int(11) DEFAULT NULL,
+  PRIMARY KEY (`empno`)
+)
---
+++ index/insert.js
@@ -0,0 +1,31 @@
+
+
+async function main() {
+    const mysql = require('mysql2/promise');
+    const connection = await mysql.createConnection({ host: 'localhost', user: 'jinyongwha', database: 'employee', password: "wlsdydghk1!" });
+    var title = ["사원", "대리", "과장", "차장", "부장"]
+    var dno = [1, 2, 3, 4]
+
+    for (var i = 0; i <= 10000000; i++) {
+        try {
+            var employee = {
+                empno: i + 1,
+                empname: "name" + i,
+                title: title[Math.floor(Math.random() * 5)],
+                dno: dno[Math.floor(Math.random() * 4)],
+                salary: Math.floor(Math.random() * 1000000) + 2000000
+
+            }
+            var sql = "insert into employee(empno, empname, title, dno, salary) values(?,?,?,?,?)"
+            var params = [employee.empno, employee.empname, employee.title, employee.dno, employee.salary]
+
+            await connection.query(sql, params)
+            console.log(i)
+        } catch (err) { }
+
+    }
+    console.log("success")
+    process.exit()
+}
+main()
+
