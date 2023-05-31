b7261fa30cd692eee4dd562b602c880ece0eed12
jinyonghwa <jinyonghwa@kakao.com>
2023-05-31 12:18:51 +0900

    Revert "add index"
    This reverts commit d7487182dcd6fd32f5e3b39bb2f50e93b3b0e8b1.

--- index/employee.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `employee` (
-  `empno` int(11) NOT NULL,
-  `empname` varchar(45) DEFAULT NULL,
-  `title` varchar(45) DEFAULT NULL,
-  `dno` varchar(45) DEFAULT NULL,
-  `salary` int(11) DEFAULT NULL,
-  PRIMARY KEY (`empno`)
-)
